4b040f5ce0b1b7faa30ef7a11b1f841371cad1ae
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2021-01-03 07:36:21 -0700

    Multiple tile support on movement command

--- actions/movement/movement.py
+++ actions/movement/movement.py
@@ -30,9 +30,21 @@ class Move(Movement):
     def execute(self):
         compass_x, compass_y = self._settings.get('POSITIONS', 'compass', True)
         tile_x, tile_y = self._settings.get('POSITIONS', f'{self.direction}_tile', True)
+        tile_x = int(tile_x)
+        tile_y = int(tile_y)
 
         super().say(f'Executing cmd: Move {self.direction} by {self.tile_count} tiles.')
         self._input.click(int(compass_x), int(compass_y))
-        sleep(0.5)
-        self._input.click(int(tile_x), int(tile_y))
+        sleep(0.3)
 
+        if self.tile_count != 1:
+            if self.direction == 'north':
+                tile_y = tile_y - (self.tile_count * 50)
+            elif self.direction == 'south':
+                tile_y = tile_y + (self.tile_count * 70)
+            elif self.direction == 'west':
+                tile_x = tile_x - (self.tile_count * 80)
+            elif self.direction == 'east':
+                tile_x = tile_x + (self.tile_count * 80)
+
+        self._input.click(tile_x, tile_y)
